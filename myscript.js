1b810c0d420b747feb62fc4ba8f696b6a0bba9e1
Maciej Sęk <115628283+MACKOzKLANU@users.noreply.github.com>
2024-01-30 15:52:12 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,15 +1,16 @@
-
-// funkcja do pokazywania/chowania teksty
+// Function for toggling password visibility
 function myFunction() {
     var x = document.getElementById("haslo");
     if (x.type === "password") {
-         x.type = "text";
+        x.type = "text";
     } else {
         x.type = "password";
-  }
+    }
 }
-//walidacja hasla
+
+// Password validation function
 function Passwordvalidate() {
+    // Retrieve password and validation criteria values
     let haslo = document.getElementById('haslo').value;
     console.log(haslo);
 
@@ -28,99 +29,81 @@ function Passwordvalidate() {
     let ilespec = document.getElementById('liczbaspecjal').value;
     console.log(ilespec);
 
+    // Split forbidden and allowed characters into arrays
     let zakazanelista = znakible.split(",");
     console.log(zakazanelista);
 
     let znakidozwolonje = document.getElementById('dozw').value;
     console.log(znakidozwolonje);
 
-    let dozwolonelista = znakidozwolonje.split(",")
+    let dozwolonelista = znakidozwolonje.split(",");
     console.log(dozwolonelista);
 
-    let liczbaznakowdozwolonych = 0
+    let liczbaznakowdozwolonych = 0;
 
-    let resultText = 'wynik: ';
-    let oki = 0
-    var i = 0
-
-    var j = 0
-
-    
+    // Initialize result text and counters
+    let resultText = 'Result: ';
+    let oki = 0;
+    var i = 0;
+    var j = 0;
 
     console.log("===========================================");
-    console.log("jest okej :)")
-    alert("Rozpoczęto walidację...");
+    console.log("Validation in progress...");
+    alert("Validation initiated...");
 
+    // Validation checks
+    if (haslo.length < minimalnaliczbaliczb) {
+        resultText += 'Password is too short, it must have at least ' + minimalnaliczbaliczb + ' characters, ';
+    } else if (haslo.length > maksymalnaliczbaliczb) {
+        resultText += 'Password is too long, ';
+    } else {
+        resultText += 'Password length is valid, ';
+        oki += 1;
+    }
 
-    //ify
-      if (haslo.length < minimalnaliczbaliczb) {
-          resultText += 'hasło jest za krótkie, hasło musi mieć conajmniej ' + minimalnaliczbaliczb + ' znakow, ' ;
-      } 
+    // Check allowed characters
+    while (i < dozwolonelista.length) {
+        if (haslo.indexOf(dozwolonelista[i]) >= 0) {
+            liczbaznakowdozwolonych += 1;
+        }
+        i += 1;
+    }
 
-      else if (haslo.length > maksymalnaliczbaliczb)
-      {
-          resultText += 'hasło jest za długie, '
-      }
+    // Check forbidden characters
+    while (j < zakazanelista.length) {
+        if (haslo.indexOf(zakazanelista[j]) >= 0) {
+            resultText += 'Password contains forbidden characters,';
+        }
+        j += 1;
+    }
 
-      else{
-          resultText += 'długość hasła jest prawidłowa, '
-          oki +=1
+    // Check uppercase letters
+    var duzzz = 0;
+    for (var i = 0; i < haslo.length; i++) {
+        if (haslo[i] >= 'A' && haslo[i] <= 'Z') {
+            duzzz++;
+        }
+    }
+    if (duzzz < duze) {
+        resultText += 'Password must contain at least ' + duze + ' uppercase letters, ';
+    } else {
+        resultText += 'Number of uppercase letters is valid, ';
+        oki += 1;
+    }
 
-      }
-      
-      
-//vary
-      
-      while(i < dozwolonelista.length){
-          if (haslo.indexOf(dozwolonelista[i]) >= 0){
-             liczbaznakowdozwolonych += 1
-          }
-          i+=1
-      }
-      
-      while(j < zakazanelista.length){
-          if (haslo.indexOf(zakazanelista[j]) >= 0){
-              resultText += 'hasło posiada znaki zakazane,'
-          }
-          j+=1
-      }
-      var duzzz = 0;
-      for (var i = 0; i < haslo.length; i++) {
-          if (haslo[i] >= 'A' && haslo[i] <= 'Z') {
-              duzzz++;
-          }
-      }
-      if (duzzz < duze) {
-          resultText += 'hasło musi zawierać co najmniej ' + duze + ' wielkich liter, ';
-      }
-      else {
-          resultText += 'ilość wielkich liter jest prawidłowa, ';
-          oki +=1
-      }
+    // Check special characters
+    if (liczbaznakowdozwolonych < ilespec) {
+        resultText += 'Password must contain at least ' + ilespec + ' special characters.';
+    } else {
+        resultText += 'Number of special characters is valid.';
+        oki += 1;
+    }
 
-      if (liczbaznakowdozwolonych < ilespec) {
-          resultText += 'hasło musi zawierać co najmniej ' + ilespec + ' specjalnych znaków.';
-      }
-      else {
-          resultText += 'ilość specjalnych znaków jest prawidłowa.';
-          oki +=1
-
-      }
-
-    //alerty
-      document.getElementById('result').innerText = resultText;
-      if(oki == 3){
-        alert("walidacja została pomyślnie zakończona hasło spełnia wymagania.");
-    
-      }
-      else if(oki < 3){
-        alert("!walidacja nie została pomyślnie zakończona hasło nie spełnia wymagań!.");
-    
-      }
-  }
-  
-  
-// skrypt jest okej 
-// function displayDate
-//document getElementById("showDate").innerHTML = Date();
-//<button type "button" onclick= "displayDate()">ShowDate</button>
+    // Display result and show appropriate alert
+    document.getElementById('result').innerText = resultText;
+    if (oki == 3) {
+        alert("Validation successfully completed. Password meets the requirements.");
+    } else if (oki < 3) {
+        alert("!Validation failed. Password does not meet the requirements!.");
+    }
+}
